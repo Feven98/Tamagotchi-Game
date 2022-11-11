68738fc12e33e8fab2e64d083a81ca0c4cfbcbf8
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2022-11-11 01:50:52 -0500

    Starting with JS code

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-// let myName = "Chimney";
-// let myAge = 6;
-
+// Instantiate class for tamagotchi(tamagotchi object)
+// window.onload = function(){
+//  let myAge= 5; //characters age
+//  let ageincrease= setInterval(myAge, 1) // incremental age of character by 1 min
+//  let name="";
+// }  
+const actionElement = document.querySelectorAll("feed");
+actionElement.forEach(feed =>{
+    feed.addEventListener('click'), handClick, {once:true}}
+    )
+// const feelings= document.getElementsById("feelings");
+// console.log(actions);
+// console.log(feelings)
 
